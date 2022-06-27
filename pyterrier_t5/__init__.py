38562c67765f50608602e6bb9ad7bc0a16664766
andreaschari <andreaschari9810@gmail.com>
2022-06-27 10:30:33 +0100

    add skeleton for mT5 support

--- pyterrier_t5/__init__.py
+++ pyterrier_t5/__init__.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from pyterrier.model import add_ranks
 import torch
 from torch.nn import functional as F
-from transformers import T5Config, T5Tokenizer, T5ForConditionalGeneration
+from transformers import T5Config, T5Tokenizer, T5ForConditionalGeneration, AutoTokenizer, AutoModelForSeq2SeqLM
 from pyterrier.transformer import TransformerBase
 from typing import List
 import re
@@ -166,3 +166,54 @@ class DuoT5ReRanker(TransformerBase):
                     v.clear()
         if len(batch['ids']) > 0:
             yield batch
+
+class mT5ReRanker(TransformerBase):
+    def __init__(self, 
+                 tok_model='unicamp-dl/mt5-base-mmarco-v2',
+                 model='unicamp-dl/mt5-base-mmarco-v2',
+                 batch_size=4,
+                 text_field='text',
+                 verbose=True):
+        self.verbose = verbose
+        self.batch_size = batch_size
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.tokenizer = AutoTokenizer.from_pretrained(tok_model)
+        self.model_name = model
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model)
+        self.model.to(self.device)
+        self.model.eval()
+        self.text_field = text_field
+        self.REL = self.tokenizer.encode('true')[0]
+        self.NREL = self.tokenizer.encode('false')[0]
+
+    def __str__(self):
+        return f"mT5({self.model_name})"
+
+    def transform(self, run):
+        scores = []
+        queries, texts = run['query'], run[self.text_field]
+        it = range(0, len(queries), self.batch_size)
+        prompts = self.tokenizer.batch_encode_plus([f'Relevant:' for _ in range(self.batch_size)], return_tensors='pt', padding='longest')
+        max_vlen = self.model.config.n_positions - prompts['input_ids'].shape[1]
+        if self.verbose:
+            it = pt.tqdm(it, desc='monoT5', unit='batches')
+        for start_idx in it:
+            rng = slice(start_idx, start_idx+self.batch_size) # same as start_idx:start_idx+self.batch_size
+            enc = self.tokenizer.batch_encode_plus([f'Query: {q} Document: {d}' for q, d in zip(queries[rng], texts[rng])], return_tensors='pt', padding='longest')
+            for key, enc_value in list(enc.items()):
+                enc_value = enc_value[:, :-1] # chop off end of sequence token-- this will be added with the prompt
+                enc_value = enc_value[:, :max_vlen] # truncate any tokens that will not fit once the prompt is added
+                enc[key] = torch.cat([enc_value, prompts[key][:enc_value.shape[0]]], dim=1) # add in the prompt to the end
+            enc['decoder_input_ids'] = torch.full(
+                (len(queries[rng]), 1),
+                self.model.config.decoder_start_token_id,
+                dtype=torch.long
+            )
+            enc = {k: v.to(self.device) for k, v in enc.items()}
+            with torch.no_grad():
+                result = self.model(**enc).logits
+            result = result[:, 0, (self.REL, self.NREL)]
+            scores += F.log_softmax(result, dim=1)[:, 0].cpu().detach().tolist()
+        run = run.drop(columns=['score', 'rank'], errors='ignore').assign(score=scores)
+        run = add_ranks(run)
+        return run
